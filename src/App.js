9900fdf27b11446aec2826b19f0891064419c9ea
Viktoria Bendikova <viktoria.bendikova@gmail.com>
2022-01-17 01:03:26 +0100

    fixed bar + docs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
-import './App.css';
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, Slider } from '@mui/material';
+import { fontFamily, fontWeight } from '@mui/system';
+import React, { useEffect, useRef, useState } from 'react';
 import Globe from 'react-globe.gl';
-import React from 'react';
-import {useState, useEffect, useRef} from 'react';
 import * as THREE from 'three';
-import {Select, InputLabel, FormControl, Button, Box, Slider} from '@mui/material';
-// import InputLabel from '@mui/material/InputLabel';
-// import FormControl from '@mui/material/FormControl';
-
-
-import { MenuItem, Fab } from '@mui/material';
-import { calculatePositions } from './data_prep';
+import './App.css';
+// import { calculatePositions } from './data_prep';
 const data_orig = require('./space_data.json');
-const processed_data = calculatePositions([...data_orig])
+var satellite = require('satellite.js');
+
+// const processed_data = calculatePositions([...data_orig])
 //const processed_data = require('./processed_data.json')
+
+function calculatePositions(data, date) {
+ 
+  //const data = JSON.parse(JSON.stringify(data_orig)); 
+
+  for (let index = 0; index < data.length; index++) {
+      const object = data[index];
+  
+      // get TLE data
+      var tleLine1 = object["TLE_LINE1"]
+      var tleLine2 = object["TLE_LINE2"]
+
+      //console.log(index)
+
+      // Initialize a satellite record
+      var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
+      //console.log(satrec)
+
+    
+  
+      
+  
+      // Get the position of the satellite at the given date
+      const date = new Date();
+      const positionAndVelocity = satellite.propagate(satrec, date);
+
+      const gmst = satellite.gstime(date);
+
+
+
+      try {
+        const position = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
+        object.lng = satellite.degreesLong(position.longitude);
+        object.lat = satellite.degreesLat(position.latitude);
+        object.alt = position.height / 1000;
+        object.radius = Math.random() * 5;
+        object.color = ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)];
+      }
+      catch (err) {
+        data.splice(index, 1);
+        continue;
+
+      }
+  
+  }
+  
+  return data;
+}
+
+const processed_data = calculatePositions([...data_orig])
+
 
 
 
@@ -34,8 +82,8 @@ function filterCountry(country, data) {
     return data
   }
 
-  return data.filter((object)=>{
-      return object["COUNTRY_CODE"] === country
+  return data.filter((object) => {
+    return object["COUNTRY_CODE"] === country
   })
 
 }
@@ -46,45 +94,49 @@ function filterType(type, data) {
     return data
   }
 
-  return data.filter((object)=>{
-      return object["OBJECT_TYPE"] === type
+  return data.filter((object) => {
+    return object["OBJECT_TYPE"] === type
   })
 
 }
 
 
-function filterLaunchDate(launchDate, data) {
+// function filterLaunchDate(launchDate, data) {
 
-  if (launchDate === 0) {
-    return data
-  }
+//   if (launchDate === 0) {
+//     return data
+//   }
 
-  return data.filter((object)=>{
-      return object["LAUNCH_DATE"] === launchDate
-  })
+//   return data.filter((object) => {
+//     return object["LAUNCH_DATE"] === launchDate
+//   })
 
-}
-
-
-function applyFilters(country, type, launchDate, data) {
-  const country_applied = filterCountry(country, data)
-  const type_applied = filterType(type, country_applied)
-  const date_applied = filterLaunchDate(launchDate, type_applied)
-  return date_applied
-
-
-}
-
-
+// }
 
 
 // timeline filtering
-function filterTimeLine(year, data) {
-  return data.filter((object)=>{
-      return object["LAUNCH_DATE"] <= year
+function filterTimeLine(yearFrom, yearTo, data) {
+  return data.filter((object) => {
+    return object["LAUNCH_DATE"] >= yearFrom && object["LAUNCH_DATE"] <= yearTo
   })
 
 }
+
+
+function applyFilters(country, type,  year, data) {
+  const country_applied = filterCountry(country, data)
+  const type_applied = filterType(type, country_applied)
+  // const date_applied = filterLaunchDate(launchDate, type_applied)
+  return filterTimeLine(year[0], year[1], type_applied)
+
+
+}
+
+
+
+
+
+
 
 
 
@@ -92,237 +144,232 @@ function filterTimeLine(year, data) {
 function App() {
 
 
-    const globeEl = useRef();
-    const [data, setData] = useState(applyFilters("PRC", "PAYLOAD", 2021, processed_data));
-    const [data2, setData2] = useState(applyFilters("PRC", "PAYLOAD", 2021, processed_data));
+  const globeEl = useRef();
+  const [data, setData] = useState([]);
+  const [pos, setPos] = useState({ x: 0, y: 0 })
+  const [test, setTest] = useState("")
+  const [country, setCountry] = useState("PRC")
+  const [objectType, setObjectType] = useState("PAYLOAD")
+  const [size, setSize] = useState("")
+  const [launchDate, setLaunchDate] = useState(2021)
+  const [year, setYear] = useState([1961,2021])
 
 
-
-    const [pos, setPos] = useState({x: 0, y: 0})
-    const [test, setTest] = useState("")
-    const [country, setCountry] = useState("PRC")
-    const [objectType, setObjectType] = useState("PAYLOAD")
-    const [size, setSize] = useState("")
-    const [launchDate, setLaunchDate] = useState(2021)
+  useEffect(() => {
+    setData(applyFilters(country, objectType,  year, processed_data))
+    console.log("useeffect")
+  }, [country, objectType,  year])
 
 
-    // useEffect(() => {
-    //   (function moveSpheres() {
-    //     data.forEach(d => d.lat += 0.2);
-    //     setData(data.slice());
-    //     requestAnimationFrame(moveSpheres);
-    //   })();
-    // }, []);
+  const handleFilter = () => {
+    setData(applyFilters(country, objectType,  year, processed_data))
+    //setData2(applyFilters(country, objectType,  processed_data))
+  }
 
-    // useEffect(() => {
-    //   globeEl.current.pointOfView({ altitude: 3.5 });
-    // }, []);
+  // useEffect(() => {
+  //   (function moveSpheres() {
+  //     data.forEach(d => d.lat += 0.2);
+  //     setData(data.slice());
+  //     requestAnimationFrame(moveSpheres);
+  //   })();
+  // }, []);
+
+  // useEffect(() => {
+  //   globeEl.current.pointOfView({ altitude: 3.5 });
+  // }, []);
 
 
 
 
 
   return (
-  
-    <>
-    {/* <div style={{backgroundColor:"red",width:"50px",left:pos.x, top:pos.y}}>
-      {test}
-    </div> */}
 
-     
+    <Box position="relative"
+      height="100%"
 
-     {/* <form onSubmit={(e) => { 
-        e.preventDefault()
-        setData(filterCountry(country))
-                              console.log(data.length)
-                              console.log(filterCountry(country).length)
-                              console.log(country) }}>
-        <label>
-          Select country code:
-          
-          <Select label="Country code" value={country} onChange={(event) => { setCountry(event.target.value)} } >
-            {
-              [...countryCodes].map( (item) => {
-                 return (<MenuItem value={item}>{item}</MenuItem>)
-              } )
-            }
-          </Select>
-          
-        </label>
-        <input type="submit" value="Submit" />
-      </form> */}
+    >
 
 
-      {/* <form onSubmit={(e) => { 
-        e.preventDefault()
-        setData(filterType(objectType))
-                              console.log(data.length)
-                              console.log(filterType(objectType).length)
-                              console.log(objectType) }}>
-        <label>
-          Select object type:
-          
-          <Select label="Object type" value={objectType} onChange={(event) => { setObjectType(event.target.value)} } >
-            {
-              [...objectTypes].map( (item) => {
-                 return (<MenuItem value={item}>{item}</MenuItem>)
-              } )
-            }
-          </Select>
-          
-        </label>
-        <input type="submit" value="Submit" />
-      </form> */}
+      <Box
+        style={{ backgroundColor: "black", backgroundColor: 'transparent' }}
+        position="absolute"
+        top={10}
+        width={200}
+        height={800}
+        zIndex={10}
+        display={"flex"}
+        flexDirection={'column'}
+        alignItems={"center"}
+
+      >
 
 
-      {/* <form onSubmit={(e) => { 
-        e.preventDefault()
-        setData(filterLaunchDate(launchDate))
-                              console.log(filterLaunchDate(launchDate).length)
-                              console.log(launchDate) }}>
-        <label>
-          Select launch date:
-          
-          <Select label="Launchdate" value={launchDate} onChange={(event) => { setLaunchDate(parseInt(event.target.value))}} >
-            
-          {
-              [...Array(61).keys()].map(i => i + 1961).map( (item) => {
-                 return (<MenuItem value={item}>{item}</MenuItem>)
-              } )
-          }
-              
-          </Select>
-          
-        </label>
-        <input type="submit" value="Submit" />
-      </form> */}
 
-        <Box style={{backgroundColor:"black"}} p={2}>
-
-        <FormControl sx={{ m: 1, minWidth: 150}}>
-        <InputLabel id="country-code" style={{color:"white"}} >Country Code</InputLabel>
-          <Select labelId="country-code" label="Country code" value={country} sx={{color:"white", "& .MuiOutlinedInput-notchedOutline": {borderColor: "white"}}}  onChange={(event) => { setCountry(event.target.value)} } >
+        <FormControl sx={{ m: 1, minWidth: 150 }}>
+          <InputLabel id="country-code" style={{ color: "white" }} >Country Code</InputLabel>
+          <Select labelId="country-code" label="Country code" value={country} sx={{ color: "white", "& .MuiOutlinedInput-notchedOutline": { borderColor: "white" } }} onChange={(event) => { setCountry(event.target.value) }} >
             <MenuItem value="ALL">
               <em>ALL</em>
             </MenuItem>
             {
-              [...countryCodes].sort().map( (item) => {
-                 return (<MenuItem value={item}>{item}</MenuItem>)
-              } )
-              
+              [...countryCodes].sort().map((item) => {
+                return (<MenuItem value={item}>{item}</MenuItem>)
+              })
+
             }
           </Select>
-          </FormControl>
+        </FormControl>
 
-          
-          <FormControl sx={{ m: 1, minWidth: 150 }}>
-          <InputLabel id="object-type" style={{color:"white"}}>Object Type</InputLabel>
-          <Select labelId="object-type" label="Object type" value={objectType} sx={{color:"white", "& .MuiOutlinedInput-notchedOutline": {borderColor: "white"}}} onChange={(event) => { setObjectType(event.target.value)} } >
+
+        <FormControl sx={{ m: 1, minWidth: 150 }}>
+          <InputLabel id="object-type" style={{ color: "white" }}>Object Type</InputLabel>
+          <Select labelId="object-type" label="Object type" value={objectType} sx={{ color: "white", "& .MuiOutlinedInput-notchedOutline": { borderColor: "white" } }} onChange={(event) => { setObjectType(event.target.value) }} >
             <MenuItem value="ALL">
               <em>ALL</em>
             </MenuItem>
             {
-              [...objectTypes].map( (item) => {
-                 return (<MenuItem value={item}>{item}</MenuItem>)
-              } )
+              [...objectTypes].map((item) => {
+                return (<MenuItem value={item}>{item}</MenuItem>)
+              })
             }
           </Select>
-          </FormControl>
+        </FormControl>
 
-          <FormControl sx={{ m: 1, minWidth: 150 }}>
-          <InputLabel id="launch-date" style={{color:"white"}}>Launch Date</InputLabel>
-          <Select labelId="launch-date" label="Launchdate" value={launchDate} sx={{color:"white", "& .MuiOutlinedInput-notchedOutline": {borderColor: "white"}}} onChange={(event) => { setLaunchDate(parseInt(event.target.value))}} >
+        {/* <FormControl sx={{ m: 1, minWidth: 150 }}>
+          <InputLabel id="launch-date" style={{ color: "white" }}>Launch Date</InputLabel>
+          <Select labelId="launch-date" label="Launchdate" value={launchDate} sx={{ color: "white", "& .MuiOutlinedInput-notchedOutline": { borderColor: "white" } }} onChange={(event) => { setLaunchDate(parseInt(event.target.value)) }} >
             <MenuItem value="0">
               <em>ALL</em>
             </MenuItem>
             {
-                [...Array(61).keys()].map(i => i + 1961).map( (item) => {
-                   return (<MenuItem value={item}>{item}</MenuItem>)
-                } )
-            }   
+              [...Array(61).keys()].map(i => i + 1961).map((item) => {
+                return (<MenuItem value={item}>{item}</MenuItem>)
+              })
+            }
           </Select>
-          </FormControl>
+        </FormControl> */}
 
 
-          <Button variant="contained" onClick={() => {
-            setData(applyFilters(country, objectType, launchDate, processed_data))
-            setData2(applyFilters(country, objectType, launchDate, processed_data))
-            console.log(country)
-            console.log(objectType)
-            console.log(launchDate)
-          }}>Apply Filters</Button>
-
+        {/* <Button variant="contained" onClick={handleFilter}>Apply Filters</Button> */}
+        <Box
+          paddingTop={10}
+          paddingLeft={5}
+          height={800}
+        >
 
           <Slider
             aria-label="Temperature"
-            defaultValue={30}
-            
-            
-            
+            defaultValue={year}
             valueLabelDisplay="on"
-            
-
-            step={5}
-            marks
+            step={1}
+            marks={[...Array(61).keys()].filter((item) => {
+              return ((item % 10 === 0) || (item === 60))
+            }).map(i => i + 1961).map((item) => {
+              return ({
+                value: item,
+                label: item
+              })
+            })}
             min={1961}
             max={2021}
+            orientation="vertical"
+            onChange={(event) => { setYear(event.target.value) }}
+            sx={{
+              '& .MuiSlider-markLabel': {
+                color: "white",
+                fontSize: 15,
+                fontWeight: "500"
+              },
+              '& .MuiSlider-valueLabel': {
 
-            onChange={(event) => { setData(filterTimeLine(event.target.value, data2)); console.log(data2) }}
+                fontSize: 12,
+                fontWeight: '500',
+                top: 23,
+                left: -56,
+                '&:before': {
+                  display: 'none',
+                },
+              },
+              '& .MuiSlider-mark': {
+                backgroundColor: '#bfbfbf',
+                height: 2,
+                width: 0.5,
+                '&.MuiSlider-markActive': {
+                  opacity: 1,
+                  backgroundColor: 'currentColor',
+                },
+              },
+              '& .MuiSlider-track': {
+                border: 'none',
+              },
+              '& .MuiSlider-rail': {
+                opacity: 0.5,
+                backgroundColor: '#bfbfbf',
+              },
+            }}
 
-            
           />
-
-          </Box>
-      
+        </Box>
 
 
-      
+      </Box>
 
 
 
-
-     
-
-    
-      
-
-
-    <Globe
-    ref={globeEl}
-    globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-    bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-
-    customLayerData={data}
-    customLayerLabel={(object) => { return  ` <b>${object.OBJECT_NAME}</b> <br /> ${object.OBJECT_ID}`}}
-    
-    customThreeObject={d => new THREE.Mesh(
-      new THREE.SphereBufferGeometry(d.radius),
-      new THREE.MeshLambertMaterial({ color: d.color })
-    )}
-    customThreeObjectUpdate={(obj, d) => {
-      Object.assign(obj.position, globeEl.current.getCoords(d.lat, d.lng, d.alt));
-      
-    }}
-
-    
-
-  
-  
-    
-    onCustomLayerClick={(obj, event)=>{
-      console.log(event)
-      setTest(JSON.stringify(obj["OBJECT_NAME"]))
-      setPos({x: event["screenX"], y: event["screenY"]})
-    }}
-    />
+      {/* <Box
+          //  style={{ backgroundColor: "black" }} 
+          position="absolute"
+          bottom={15}
+          zIndex={10}
+          width={1000}
+          marginLeft={"-500px"}
+          left="50%"
+          
+        >
+         
+        </Box> */}
 
 
-    </>
-
-  
 
 
-  
+
+
+
+
+
+      <Box height="100%">
+        <Globe
+          // height={1080}
+          ref={globeEl}
+          globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+          bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+
+          customLayerData={data}
+          customLayerLabel={(object) => { return ` <b>${object.OBJECT_NAME}</b> <br /> ${object.OBJECT_ID}` }}
+
+          customThreeObject={d => new THREE.Mesh(
+            new THREE.SphereBufferGeometry(d.radius),
+            new THREE.MeshLambertMaterial({ color: d.color })
+          )}
+          customThreeObjectUpdate={(obj, d) => {
+            Object.assign(obj.position, globeEl.current.getCoords(d.lat, d.lng, d.alt));
+
+          }}
+          onCustomLayerClick={(obj, event) => {
+            console.log(event)
+            setTest(JSON.stringify(obj["OBJECT_NAME"]))
+            setPos({ x: event["screenX"], y: event["screenY"] })
+          }}
+        />
+      </Box>
+
+
+    </Box>
+
+
+
+
+
 
   );
 }
